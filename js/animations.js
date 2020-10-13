4565ddd2fb84ba647dc9f26882098fabba01f2f5
Liem Nguyen <liemnguyen400@gmail.com>
2020-10-13 14:35:10 -0400

    dress picture fix and sale prices added

--- js/animations.js
+++ js/animations.js
@@ -10,24 +10,3 @@ hamburgerIcon.addEventListener('click', () => {
 document.addEventListener('click', () => {
   nav.classList.remove('open');
 });
-
-/* small screen and mobile slideshow */
-var index = 0;
-
-function slideshow() {
-  const slides = document.getElementsByClassName("slide");
-  for (var i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  
-  index++;
-  
-  if (index > slides.length) { 
-    index = 1;
-  }
-
-  slides[index - 1].style.display = "block";
-  setTimeout(slideshow, 3000);
-}
-
-slideshow();
